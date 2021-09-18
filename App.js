0dcffde4e916849f82dcf28a0b557e8868034a9a
hashika <m.g.hashikamaduranga@gmail.com>
2021-09-18 23:53:30 +0530

    changes

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,  TouchableOpacity } from 'react-native';
+// import Navigator from './routes/homeStack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/home/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Store from './components/store/Store';
+import StackHeader from './util/StackHeader';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return ( <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"
+        headerMode="screen"
+        screenOptions={{
+          header:({scene,navigation})=>(
+            <StackHeader scene={scene} navigation={navigation}/>
+          )
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Store" component={Store} />
+      </Stack.Navigator>
+    </NavigationContainer>)
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +36,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  homeTitle:{
+    fontSize: 25,
+    fontWeight: "bold",
+    color:"green",
+    // fontFamily:"fangsong"
+  },
+   image :{
+    width: 250,
+    height: 400,
+    marginBottom: 40,
+    marginTop: 100,
+    marginLeft:20,
+    marginRight: 20
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "green",
+    color:"white"
+  },
+  loginText:{
+    fontSize: 20,
+    fontWeight: "bold",
+    color:"white",
+    // fontFamily:"fangsong"    
+  }
 });
